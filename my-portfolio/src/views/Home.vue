58e78136224ef5be50443365fd8588990e90feb8
cheulong <searcheulong@gmail.com>
2018-06-08 15:00:07 +0700

    project section

--- my-portfolio/src/views/Home.vue
+++ my-portfolio/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Hero />
+    <Project />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Hero from '@/components/Hero.vue'
+import Project from '@/components/Project.vue'
 
 export default {
   name: 'home',
   components: {
-    Hero
+    Hero,
+    Project
   }
 }
 </script>
