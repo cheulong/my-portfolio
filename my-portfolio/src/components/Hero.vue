c1c4aeffaa00b8d8699b13bfabc06f3834a86e20
cheulong <searcheulong@gmail.com>
2018-06-08 14:57:17 +0700

    add hero section

---
+++ my-portfolio/src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hero">
+    <h1>Cheulong<br>Sear</h1>
+    <p>I’m a Software Engineering student at CAMT, CMU</p>
+    <p>Looking for a internship.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- my-portfolio/src/views/Home.vue
+++ my-portfolio/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Hero from '@/components/Hero.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Hero
   }
 }
 </script>
